e5a17b81573a4dc792dadf47b41a664e57596f77
ckopecky <kopecky12112@gmail.com>
2019-08-12 09:30:22 -0700

    refactor using Math.sign()

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -13,22 +13,31 @@ function reverseInt(n) {
     const reversed = n.toString().split('').reverse().join('');
     return parseInt(reversed) * Math.sign(n);
 }
-
-
 */
 
-function reverseInt(n) {
-    let num = n + '';
-    if(n < 0){
-        let split = num.split('').reverse();
-        let sign = split.pop();
-        split.unshift(sign);
-        let join = split.join('');
-        return Number(join);
-    }
+// function reverseInt(n) {
+//     let num = n + '';
+//     if(n < 0){
+//         let split = num.split('').reverse();
+//         let sign = split.pop();
+//         split.unshift(sign);
+//         let join = split.join('');
+//         return Number(join);
+//     }
     
-    return Number(num.split('').reverse().join(''));
+//     return Number(num.split('').reverse().join(''));
 
+// }
+
+const reverseInt = num => {
+    let sign = Math.sign(num);
+    if(sign === 1) {
+        return Number(num.toString().split('').reverse().join(''));
+    } else {
+        let split = num.toString().split('');
+        split.shift();
+        return Number(split.reverse().join('')) * sign;
+    }
 }
 
 module.exports = reverseInt;
