f8c8ff1f3bb70d6348543d886a02cde3327d06d7
ckopecky <kopecky12112@gmail.com>
2019-08-12 09:31:19 -0700

    add object version of stack in comments

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -13,18 +13,30 @@
 class Stack {
     constructor() {
         this.stack = [];
+        //this.storage = {};
+        //this.count = 0;
     }
 
     push(item) {
-        this.stack.push(item);
+        this.stack = [this.stack, ...item];
+        /*
+        
+        this.stack[this.count] = item;
+        this.count++
+        */
     }
 
     pop() {
         return this.stack.pop();
+        // let popped = this.stack[this.count];
+        // delete this.stack[this.count];
+        // return popped;
+
     }
 
     peek() {
         return this.stack[this.stack.length - 1];
+        //return this.stack[this.count]
     }
 }
 
