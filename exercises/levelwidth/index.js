1227e305370518edb7de7edd5ccc924ae5955db6
ckopecky <kopecky12112@gmail.com>
2019-08-12 14:42:20 -0700

    breadth first level prob

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,42 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+//anytime you see width, you want to start with a BF traversal of the tree
+
+//maintain a counter array and a queue with children
+let counter = [0];
+let level = 0;
+let stop = 'stop';
+let arr = [root, stop]
+
+    while(arr.length) {
+        //take out first node
+        let first = arr.shift();
+        //take children and add to array
+        //increment counter by 1
+        //throw root out
+        //next is stopper -- reinsert it to end of array and add another index to counter array and instantiate it to 0
+
+        if(first === stop && arr.length > 0) {
+            arr = [...arr, first];
+            counter = [...counter, 0]
+            level += 1;
+        } 
+        if(!first.children) {
+            counter[level] += 0;
+        } else {
+            if(first === stop) {
+                return;
+            }
+            arr =[...arr, ...first.children];
+            counter[level] += 1;
+            console.log(arr, counter, "after adding children");
+
+        }
+    }
+    return counter;
+}
+
 
 module.exports = levelWidth;
--- exercises/levelwidth/test.js
+++ exercises/levelwidth/test.js
@@ -6,14 +6,16 @@ test('levelWidth is a function', () => {
 });
 
 test('levelWidth returns number of nodes at widest point', () => {
-  const root = new Node(0);
-  root.add(1);
-  root.add(2);
-  root.add(3);
-  root.children[0].add(4);
-  root.children[2].add(5);
+  const root = new Node(20);
+  root.add(0);
+  root.add(40);
+  root.add(-15);
+  root.children[0].add(12);
+  root.children[0].add(-2);
+  root.children[0].add(1);
+  root.children[2].add(-2)
 
-  expect(levelWidth(root)).toEqual([1, 3, 2]);
+  expect(levelWidth(root)).toEqual([1, 3, 4]);
 });
 
 test('levelWidth returns number of nodes at widest point', () => {
